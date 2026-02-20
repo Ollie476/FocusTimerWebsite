44315484da1894066c80b51601cd5b3e0fcf4e55
Ollie <olibobadventuresyt@gmail.com>
2026-02-20 15:30:07 +0000

    triple equals instead of double

--- calendar.js
+++ calendar.js
@@ -55,7 +55,7 @@ function updateMonth() {
             const day = splitDate[0];
             console.log(day);
 
-            if (day == i) {
+            if (day === i) {
                 isBlank = false;
                 break;
             }
